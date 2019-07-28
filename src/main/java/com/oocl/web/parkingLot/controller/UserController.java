e331246837c91656a3260a014b4b661aedd9589a
TANGLO2 <1614468806@qq.com>
2019-07-28 22:06:07 +0800

    add:cross origin in UserController

--- src/main/java/com/oocl/web/parkingLot/controller/UserController.java
+++ src/main/java/com/oocl/web/parkingLot/controller/UserController.java
@@ -5,6 +5,7 @@ import com.oocl.web.parkingLot.entity.User;
 import com.oocl.web.parkingLot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin(origins ="*")
 public class UserController {
 
     private static final String CURRENT_USER= "CURRENT_USER";
