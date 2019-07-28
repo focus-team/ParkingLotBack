f00910d60921fea6e6b4e87db9fe5372508ce38f
LOUIS TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGLO2@OOCL.COM>
2019-07-28 20:37:59 +0800

    add: identify verifycation feature

---
+++ src/main/java/com/oocl/web/parkingLot/tool/IdentifyVerifycation.java
@@ -0,0 +1,18 @@
+package com.oocl.web.parkingLot.tool;
+
+import com.oocl.web.parkingLot.entity.User;
+
+import javax.servlet.http.HttpSession;
+
+public class IdentifyVerifycation {
+
+    /***
+     *
+     * @param httpSession
+     * @param key
+     * @return null if current session does not store use info, or return current login user info.
+     */
+    public User verify(HttpSession httpSession, String key){
+        return (User) httpSession.getAttribute(key);
+    }
+}
