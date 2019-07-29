3e1f6b08b2646a3586e7468283c3d47285ed8653
XUBE8 <BERIO.XU@OOCL.COM>
2019-07-29 20:04:45 +0800

    Refactor: refactor user controller.

--- src/main/java/com/oocl/web/parkingLot/controller/UserController.java
+++ src/main/java/com/oocl/web/parkingLot/controller/UserController.java
@@ -1,8 +1,15 @@
 package com.oocl.web.parkingLot.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.oocl.web.parkingLot.common.IdentifyVerifycation;
 import com.oocl.web.parkingLot.common.ResponseStatus;
+import com.oocl.web.parkingLot.common.ServerResponse;
+import com.oocl.web.parkingLot.common.TagConst;
 import com.oocl.web.parkingLot.entity.User;
 import com.oocl.web.parkingLot.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,20 +22,25 @@ import javax.servlet.http.HttpSession;
 @RestController
 @RequestMapping("/user")
 @CrossOrigin(origins ="*")
+@Api(value = "UserApi",description = "用户相关接口")
 public class UserController {
 
-    private static final String CURRENT_USER= "CURRENT_USER";
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @Autowired
     private UserService userServiceImpl;
 
+
+    @ApiOperation(value = "用户相关接口: 用户登录")
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody User user, HttpServletRequest request) {
+    public ServerResponse<User> login(@ApiParam("用户对象") @RequestBody User user) {
         User u =  userServiceImpl.findUserByUserNameAndPassword(user.getUserName(), user.getPassword());
         if(u == null){
-            return ResponseEntity.status(ResponseStatus.NOT_FOUND.getStatusCode()).body(ResponseStatus.NOT_FOUND.getStatusDesc());
+            return ServerResponse.createByErrorCodeAndMessage(ResponseStatus.NOT_FOUND.getStatusCode(), ResponseStatus.NOT_FOUND.getStatusDesc());
         }
-        request.getSession().setAttribute(CURRENT_USER, u);
-        return ResponseEntity.ok(ResponseStatus.SUCCESS.getStatusDesc());
+        return ServerResponse.createBySuccess(IdentifyVerifycation.storeUser(u), u);
     }
+
+
+
 }
