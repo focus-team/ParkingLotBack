41cfb1c9c8316c8eca07555ed152394d607fd65b
TANGLO2 <1614468806@qq.com>
2019-07-29 09:18:46 +0800

    implement:should_return_success_when_login_given_right_username_and_password

--- src/test/java/com/oocl/web/parkingLot/controller/UserControllerTest.java
+++ src/test/java/com/oocl/web/parkingLot/controller/UserControllerTest.java
@@ -1,28 +1,46 @@
 package com.oocl.web.parkingLot.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.oocl.web.parkingLot.entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-class UserControllerTest {
+public class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    private ObjectMapper mapper = new ObjectMapper();
+
     @Test
-    public void should_return_success_when_login_given_right_username_and_password(){
+    public void should_return_success_when_login_given_right_username_and_password() throws Exception {
+        User user = new User();
+        user.setUserName("admin");
+        user.setPassword("admin");
 
+        String content = mapper.writeValueAsString(user);
 
-//        mockMvc.perform("/user/login")
-//                .andExpect()
+        final RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/user/login")
+                .content(content)
+                .contentType(MediaType.APPLICATION_JSON);
 
+        mockMvc.perform(requestBuilder)
+                .andDo(print())
+                .andExpect(status().isOk());
 
     }
 }
