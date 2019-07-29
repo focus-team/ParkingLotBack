f469a2b490f0ab957c69040de44205ef7ff667b0
XUBE8 <BERIO.XU@OOCL.COM>
2019-07-29 14:38:32 +0800

    Test: test httpSession for browser.

--- src/main/java/com/oocl/web/parkingLot/controller/UserController.java
+++ src/main/java/com/oocl/web/parkingLot/controller/UserController.java
@@ -3,6 +3,8 @@ package com.oocl.web.parkingLot.controller;
 import com.oocl.web.parkingLot.common.ResponseStatus;
 import com.oocl.web.parkingLot.entity.User;
 import com.oocl.web.parkingLot.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +17,7 @@ import javax.servlet.http.HttpSession;
 public class UserController {
 
     private static final String CURRENT_USER= "CURRENT_USER";
+
     @Autowired
     private UserService userServiceImpl;
 
@@ -24,6 +27,7 @@ public class UserController {
         if(u == null){
             return ResponseEntity.status(ResponseStatus.NOT_FOUND.getStatusCode()).body(ResponseStatus.NOT_FOUND.getStatusDesc());
         }
+        System.out.println("Session: " + httpSession + " store successfully login user.");
         httpSession.setAttribute(CURRENT_USER, u);
         return ResponseEntity.ok(ResponseStatus.SUCCESS.getStatusDesc());
     }
