b26e9d94e1604ece775c40fc0ef68252a3e7d32f
AndersonBlog <anderson992@163.com>
2019-08-01 21:58:05 +0800

    fix bug

--- src/main/java/com/oocl/web/parkingLot/service/impl/ParkingCarStrategyA.java
+++ src/main/java/com/oocl/web/parkingLot/service/impl/ParkingCarStrategyA.java
@@ -149,17 +149,19 @@ public class ParkingCarStrategyA implements ParkingCarStrategy {
                 OrderDTO orderDTO = new OrderDTO(savedParkingOrder);
                 return ResponseEntity.ok().body(orderDTO);
             } else {
-                Map<String, String> data = new HashMap<String, String>();
 
-                data.put("code", "2");
-                data.put("errMessage","AvailableParkingBoys has too many order pending.System can't dispatch!Waiting for pick-uping by hand!");
-                return ResponseEntity.ok().body(new GlobalException(2, "AvailableParkingBoys has too many order pending.System can't dispatch!Waiting for pick-uping by hand!",data));
+                Map<String, String> data = new HashMap<String, String>();
+                data.put("code", "5");
+                data.put("errMessage","There has no spare parkingLot!Please wait for "+ forecastTimeForFreeParkingSpaces + " minutes,thanks!");
+                return ResponseEntity.ok().body(new GlobalException(5, "There has no spare parkingLot!Please wait for " + forecastTimeForFreeParkingSpaces + " minutes,thanks!",data));
+
             }
         }
         Map<String, String> data = new HashMap<String, String>();
-        data.put("code", "5");
-        data.put("errMessage","There has no spare parkingLot!Please wait for "+ forecastTimeForFreeParkingSpaces + " minutes,thanks!");
-        return ResponseEntity.ok().body(new GlobalException(5, "There has no spare parkingLot!Please wait for " + forecastTimeForFreeParkingSpaces + " minutes,thanks!",data));
+
+        data.put("code", "2");
+        data.put("errMessage","AvailableParkingBoys has too many order pending.System can't dispatch!Waiting for pick-uping by hand!");
+        return ResponseEntity.ok().body(new GlobalException(2, "AvailableParkingBoys has too many order pending.System can't dispatch!Waiting for pick-uping by hand!",data));
     }
 
     private Date splitDateString(String startTime) {
